89648ceb7e03ada122c5e1aeee6ded69acb552cf
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2024-09-15 16:28:27 -0300

    DepartmentDaoJDBC implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -15,12 +15,28 @@ public class Program2 {
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
 		
-		System.out.println("\n=== TEST 2: department findAll ===");
+		//System.out.println("\n=== TEST 2: department insert ===");
+		//Department dp = new Department(null, "Phone");
+		//departmentDao.insert(dp);
+		
+		//System.out.println("\n=== TEST 3: department update ===");
+		//dep = departmentDao.findById(5);
+		//dep.setName("Smartphone");
+		//departmentDao.update(dep);
+		
+	
+		System.out.println("\n=== TEST 4: department delete ===");
+		departmentDao.deleteById(6);
+		System.out.println("Done\n");
+		
+		
+		System.out.println("\n=== TEST 5: department findAll ===");
 		List<Department> list = departmentDao.findAll();
 		
 		for(Department dept : list) {
 			System.out.println(dept);
 		}
+		
 	}
 
 }
